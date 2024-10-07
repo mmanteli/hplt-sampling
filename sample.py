7229a94bff789cc725279e3519609f1b9fc96b0c
mmanteli <amanda.myntti@gmail.com>
2024-10-07 12:19:39 +0300

    remove occasional ascii error

--- sample.py
+++ sample.py
@@ -1,7 +1,11 @@
 import json
 import sys
 import random
+import io
 limit = float(sys.argv[1])
+
+sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='ascii', errors='replace')  # 'replace' handles non-ASCII characters
+
 
 # PIPED INPUT
 for line in sys.stdin:
