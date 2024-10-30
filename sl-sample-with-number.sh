b01902c3fb31e2de4a34dc6437e915751aa1dbec
mmanteli <amanda.myntti@gmail.com>
2024-10-30 13:09:29 +0200

    modified for multiple temps and predownload

--- sl-sample-with-number.sh
+++ sl-sample-with-number.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=sample
-#SBATCH --account=project_2005092
-#SBATCH --partition=medium
-#SBATCH --time=10:00:00
+#SBATCH --account=project_462000444   # for resource and queue efficiency
+#SBATCH --partition=small
+#SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 ##SBATCH --hint=nomultithread
@@ -10,6 +10,8 @@
 #SBATCH -o logs/%j.out
 #SBATCH -e logs/%j.err
 
+module load LUMI
+module load wget
 # Input parameters
 version=$1
 START=$2
@@ -31,28 +33,28 @@ fi
 case $version in
     1_0)
         url_stem="https://data.hplt-project.org/one/monotext/en/#.jsonl.zst"
-        output_path="/scratch/project_2005092/amanda/mahti-tokenisation/results/v_1_0"
-        probability_row=0.023  # these all have +0.002 so that we go over 300B tokens
+        output_path="/scratch/project_462000353/amanda/sampling/results/v_1_0"
+        probability_row=0.0256  # these all have +0.002 so that we go over 300B tokens
         ;;
     1_1)
         url_stem="https://data.hplt-project.org/one/monotext/deduplicated/en/en_#.jsonl.zst"
-        output_path="/scratch/project_2005092/amanda/mahti-tokenisation/results/v_1_1"
-        probability_row=0.078
+        output_path="/scratch/project_462000353/amanda/sampling/results/v_1_1"
+        probability_row=0.0918 
         ;;
     1_2)
         url_stem="https://data.hplt-project.org/one/monotext/cleaned/en/en_#.jsonl.zst"
-        output_path="/scratch/project_2005092/amanda/mahti-tokenisation/results/v_1_2"
-        probability_row=0.098
+        output_path="/scratch/project_462000353/amanda/sampling/results/v_1_2"
+        probability_row=0.1139
         ;;
     2_0 | 2_0cleaned)
         url_stem="https://data.hplt-project.org/two/cleaned/eng_Latn/#.jsonl.zst"
-        output_path="/scratch/project_2005092/amanda/mahti-tokenisation/results/v_2_0"   # basename of this added to logs
-        probability_row=0.079
+        output_path="/scratch/project_462000353/amanda/sampling/results/v_2_0"   # basename of this added to logs
+        probability_row=0.0919
         ;;
     2_0dedup)
         url_stem="https://data.hplt-project.org/two/deduplicated/eng_Latn/#.jsonl.zst"
-        output_path="/scratch/project_2005092/amanda/mahti-tokenisation/results/v_2_0_dedup"
-        probability_row=0.065
+        output_path="/scratch/project_462000353/amanda/sampling/results/v_2_0_dedup"
+        probability_row=0.0701
         ;;
     *)
     echo "Version number not given correctly"
@@ -61,30 +63,34 @@ case $version in
 esac
 
 
-mkdir -p temp
+mkdir -p temp_${version}
 mkdir -p $output_path
 
-echo "Loading module"
+echo "Loading parallel module"
 module load parallel
 
 # Main script logic
 for ((i=START;i<=END;i++)); do
     # get the url from which to download
     url=${url_stem/"#"/$i}
-    echo "In ${url}"
+    echo "In ${url} $(date +"%T")"
     p=$(basename $url)    # for saving in temp => multiple jobs can run simultaneously if the files are called different
     bname="${p%.*}"     # for outfile without .zst => changing to .gz
-    if wget --no-verbose -O temp/$p $url; then
-        echo "Download succeeded: $p $(date +"%T")"
+    if [[ -f "temp_${version}/$p" ]]; then
+        echo "File already exists in temp: $p, skipping download $(date +"%T")"
     else
-        echo "Download failed: $p $(date +"%T")"
-        continue  # Skip to the next iteration if wget fails
+        if wget --no-verbose -O temp_${version}/$p $url; then
+            echo "Download succeeded: $p $(date +"%T")"
+        else
+            echo "Download failed: $p $(date +"%T")"
+            continue  # Skip to the next iteration if wget fails
+        fi
     fi
     # process in parallel
-    echo "processing temp/${p}"
-    zstdcat "temp/$p" | parallel --pipe -j64 -k --block 10M python3 sample.py $probability_row | pigz > "${output_path}/${bname}.gz"
+    echo "processing temp_${version}/${p}"
+    zstdcat "temp_${version}/$p" | parallel --pipe -j64 --block 10M python3 sample.py $probability_row | pigz > "${output_path}/${bname}.gz"
     echo "success with ${p} $(date +"%T")"
-    rm temp/$p
+    rm temp_${version}/$p
 done
 
 
